1404964bfa9ba6c1bc583c56cd08c2838eb6c74c
Hak_Kimly <kimly.hak@student.cadt.edu.kh>
2025-10-30 22:25:09 +0700

    Add files via upload

---
+++ week4/Ex1.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text(
+            "Hello My name is Kimly",
+            style: TextStyle(
+              color: Colors.green,
+              fontSize: 50
+            ),
+          ),
+        ),
+      )
+    )
+  );
+}
